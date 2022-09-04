ec117545e86b97ade4d49ac8fcc994f4217a3335
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2022-09-04 17:09:23 +0200

    test mail

--- netlify/functions/notify.js
+++ netlify/functions/notify.js
@@ -1,22 +1,47 @@
 const controller = require('../../controller')
 
+const nodemailer = require('nodemailer')
+
+const {
+    EXPEDITOR_EMAIL,
+    EXPEDITOR_PASSWORD,
+    SMTP_HOST,
+    SMTP_PORT
+} = require('../../config')
+
+const transporter = nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    auth: {
+        user: EXPEDITOR_EMAIL,
+        pass: EXPEDITOR_PASSWORD,
+    }
+})
+
 exports.handler = async function (event, context) {
     let body = '';
 
-    const request = {
-        query: {
-            email: event.queryStringParameters.email
-        }
+    const mail = {
+        recipient: event.queryStringParameters.email,
+        subject: 'toto',//data.title,
+        content: 'test'//data.content
     };
 
-    const response = {
-        send: (result) => {
-            body = result;
-            console.log('result', result);
-        }
-    };
+    try {
+        const result = await transporter.sendMail({
+            from: EXPEDITOR_EMAIL,
+            to: mail.recipient,
+            subject: mail.subject,
+            html: mail.content
+        })
+
+        console.log('result mail', result, mail)
     
-    await controller.notify(request, response);
+        success('Mail sent to ' + mail.recipient)
+    }
+    catch(error) {
+        failure(error)
+    }
 
     return {
         statusCode: 200,
