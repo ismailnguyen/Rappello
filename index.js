f93a5da49b70fd24d9be39dbaf88a89c698bd858
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2022-09-04 17:32:43 +0200

    add comments

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const controller = require('./controller')
 const { PORT } = require('./config');
 
 /*
- * This endpoint will send a mail to the recipient to ask to acknowledge if he is still alive
+ * This endpoint will send a mail to the recipient with detail of latest RSS feed from rappel.conso.gouv.fr
 */
-app.get('/notify', controller.notify)
+app.get('/notify', (request, response) => {
+  const result = controller.notify(request.query.email);
+
+  response.status(200).json(result);
+})
 
 app.listen(PORT, () => {
   console.log(`App now running on port ${PORT}`)
--- netlify/functions/notify.js
+++ netlify/functions/notify.js
@@ -1,5 +1,8 @@
 const controller = require('../../controller')
 
+/*
+ * This endpoint will send a mail to the recipient with detail of latest RSS feed from rappel.conso.gouv.fr
+*/
 exports.handler = async function (event, context) {
     return {
         statusCode: 200,
