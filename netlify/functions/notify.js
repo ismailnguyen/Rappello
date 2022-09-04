d63d536abe2355350050679325606ebc23963288
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2022-09-04 16:27:28 +0200

    test axios

--- netlify/functions/notify.js
+++ netlify/functions/notify.js
@@ -15,6 +15,14 @@ exports.handler = async function (event, context) {
             console.log('result', result);
         }
     };
+
+
+        console.log('calling axios from start point')
+    const axios = require('axios')
+    axios
+    .get('https://rappel.conso.gouv.fr/rss')
+    .then((response) => { return console.log(response.data) })
+  .catch((error) => { return console.log(error) })
     
     controller.notify(request, response);
 
